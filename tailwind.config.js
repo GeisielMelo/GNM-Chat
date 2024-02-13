f8b92de7ec617f28c7aee20defcb4d7d4d810695
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2024-02-13 20:48:18 -0300

    raw: tailwind config file

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+
+import defaultTheme from "tailwindcss";
+
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    screens: {
+      xs: "320px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      ...defaultTheme.screens,
+    },
+    extend: {},
+  },
+  plugins: [],
+};
