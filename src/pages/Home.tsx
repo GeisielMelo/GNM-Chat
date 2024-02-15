c591274db44941da8fb6ed5de6d0605e5e47a407
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2024-02-15 15:35:44 -0300

    feat: add chat components in Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,14 @@
+import Chat from '../components/Chat/Chat'
+import Sidebar from '../components/Sidebar/Sidebar'
+
 const Home: React.FC = () => {
   return (
-    <div>Home</div>
+    <section className='flex justify-center items-center h-[100dvh] p-2'>
+      <div className='flex w-full h-full rounded-md border border-zinc-400 bg-white'>
+        <Sidebar />
+        <Chat />
+      </div>
+    </section>
   )
 }
 export default Home
