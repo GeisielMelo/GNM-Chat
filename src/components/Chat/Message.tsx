efa0791bbd56185201ef0b24222345f2cd4e3430
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2024-02-14 23:42:36 -0300

    feat: message component

---
+++ src/components/Chat/Message.tsx
@@ -0,0 +1,9 @@
+const Message: React.FC = () => {
+  return (
+    <div className='flex items-end gap-4 mt-2 p-2 max-w-max rounded bg-green-300'>
+      <p>Hello</p>
+      <p className='text-xs max-h-4'>05:22 PM</p>
+    </div>
+  )
+}
+export default Message
