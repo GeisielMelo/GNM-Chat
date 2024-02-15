9a5ee4eea1a0bbd4186af1a03168f7fcbc5abd82
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2024-02-14 23:45:12 -0300

    feat: add search bar

---
+++ src/components/Sidebar/Search.tsx
@@ -0,0 +1,10 @@
+const Search: React.FC = () => {
+  return (
+    <input
+      className='w-full pl-2 border border-zinc-400 rounded'
+      type='text'
+      placeholder={'Search or start a new chat.'}
+    />
+  )
+}
+export default Search
