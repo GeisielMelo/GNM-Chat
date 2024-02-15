912cbb3beda4d235fdbe57e00d538566a9fa207e
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2024-02-14 23:53:16 -0300

    feat: add wrapper for contact component

---
+++ src/components/Sidebar/Chats.tsx
@@ -0,0 +1,21 @@
+import Contact from './Contact'
+
+const Chats: React.FC = () => {
+  return (
+    <div className='p-2 h-[calc(100%-67px)] overflow-y-scroll'>
+      <Contact />
+      <Contact />
+      <Contact />
+      <Contact />
+      <Contact />
+      <Contact />
+      <Contact />
+      <Contact />
+      <Contact />
+      <Contact />
+      <Contact />
+    </div>
+  )
+}
+
+export default Chats
