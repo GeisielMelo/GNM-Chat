0555b639d92e2afe9544934e57443b6502feec5c
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2024-02-14 23:44:56 -0300

    feat: component to show the selected contact

---
+++ src/components/Chat/Person.tsx
@@ -0,0 +1,9 @@
+const Person = () => {
+  return (
+    <div className="flex p-2 gap-2 border border-b-zinc-400">
+      <img className='w-6 h-6 rounded-full object-cover' src='' alt='user image' />
+      <h1>Lorem da silva</h1>
+    </div>
+  )
+}
+export default Person
