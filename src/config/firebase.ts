67cfee6b42de2b1edcc9d24aefcce29d92e3ee49
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2024-02-15 15:33:37 -0300

    feat: firebase configuration file

---
+++ src/config/firebase.ts
@@ -0,0 +1,27 @@
+import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
+
+type TOptions = {
+  apiKey: string
+  authDomain: string
+  projectId: string
+  storageBucket: string
+  messagingSenderId: string
+  appId: string
+}
+
+const options: TOptions = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGE_SENDER,
+  appId: import.meta.env.VITE_FIREBASE_APPID,
+}
+
+const app = initializeApp(options)
+export const auth = getAuth(app)
+export const storage = getStorage(app)
+export const database = getFirestore(app)
