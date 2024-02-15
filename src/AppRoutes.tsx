47d4b07f7f14c8db483f0971d921e5417ff2eb9b
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2024-02-15 15:33:16 -0300

    feat: add routes components

---
+++ src/AppRoutes.tsx
@@ -0,0 +1,38 @@
+import { ReactNode } from 'react'
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
+import { useAuth } from './context/AuthContext'
+
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Main from './pages/Main'
+
+type RoutesProps = {
+  children: ReactNode
+}
+
+const AppRoutes: React.FC = () => {
+  const { authenticated, loading } = useAuth()
+
+  const Private: React.FC<RoutesProps> = ({ children }) => {
+    return loading ? <span>Loading...</span> : authenticated ? children : <Navigate to='/sign-in' />
+  }
+
+  const Public: React.FC<RoutesProps> = ({ children }) => {
+    return loading ? <span>Loading...</span> : authenticated ? <Navigate to='/' /> : children
+  }
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={'/'} element={<Main />} />
+        <Route path={'home'} element={<Private><Home /></Private>}/>
+        <Route path={'sign-in'} element={<Public><Login /></Public>}/>
+        <Route path={'sign-up'} element={<Public><Register /></Public>}/>
+      </Routes>
+    </BrowserRouter>
+  )
+
+}
+
+export default AppRoutes
