15eefe2566abcce2b6e10afebf49161abacd227a
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2024-02-14 23:42:48 -0300

    feat: message box

---
+++ src/components/Chat/Messages.tsx
@@ -0,0 +1,31 @@
+import Message from './Message'
+
+function Messages() {
+  return (
+    <div className='flex flex-col p-2 h-[calc(100%-90px)] overflow-y-scroll'>
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+    </div>
+  )
+}
+export default Messages
