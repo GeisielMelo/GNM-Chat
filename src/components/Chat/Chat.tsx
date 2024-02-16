6df7289075ad06512290210e00646bd291478cba
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2024-02-16 14:19:55 -0300

    feat: updated Chat component with useChat context

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -1,14 +1,18 @@
+import { useChat } from '../../context/ChatContext'
 import Input from './Input'
 import Messages from './Messages'
 import Person from './Person'
 
 const Chat: React.FC = () => {
+  const { data } = useChat()
+
   return (
     <div className='w-full'>
-      <Person />
+      <Person avatar={data?.user.photoURL} name={data?.user.displayName} />
       <Messages />
       <Input />
     </div>
   )
 }
+
 export default Chat
