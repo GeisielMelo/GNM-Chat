65a02bc87606e9db229af912b78620eb49b26907
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2024-02-16 14:14:35 -0300

    feat: using context on chats

--- src/components/Sidebar/Chats.tsx
+++ src/components/Sidebar/Chats.tsx
@@ -1,19 +1,54 @@
+import { useEffect, useState } from 'react'
+import { useAuth } from '../../context/AuthContext'
+import { doc, onSnapshot } from 'firebase/firestore'
+import { database } from '../../config/firebase'
 import Contact from './Contact'
+import { useChat } from '../../context/ChatContext'
+
+type ChatsProps = {
+  photoURL: string
+  displayName: string
+  text?: string
+}
 
 const Chats: React.FC = () => {
+  const { user } = useAuth()
+  const { dispatch } = useChat()
+
+  const [chats, setChats] = useState<ChatsProps>([])
+
+  useEffect(() => {
+    const getChats = () => {
+      if (!user) return
+      const unsub = onSnapshot(doc(database, 'userChats', user.uid), (doc) => {
+        setChats(doc.data())
+      })
+
+      return () => {
+        unsub()
+      }
+    }
+
+    user && getChats()
+  }, [user])
+
+  const handleSelect = (u) => {
+    dispatch({ type: 'CHANGE_USER', payload: u })
+  }
+
   return (
     <div className='p-2 h-[calc(100%-67px)] overflow-y-scroll'>
-      <Contact />
-      <Contact />
-      <Contact />
-      <Contact />
-      <Contact />
-      <Contact />
-      <Contact />
-      <Contact />
-      <Contact />
-      <Contact />
-      <Contact />
+      {Object.entries(chats)
+        ?.sort((a, b) => b[1].date - a[1].date)
+        .map((_, key) => (
+          <Contact
+            key={key}
+            avatar={_[1].userInfo.photoURL}
+            name={_[1].userInfo.displayName}
+            message={_[1].lastMessage?.text}
+            onClick={() => handleSelect(_[1].userInfo)}
+          />
+        ))}
     </div>
   )
 }
