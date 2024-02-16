2e4a2fd0ee8d9e880ffe95b106bbf13dfc60b8be
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2024-02-16 14:19:31 -0300

    feat: updated messager component with chatcontext

--- src/components/Chat/Messages.tsx
+++ src/components/Chat/Messages.tsx
@@ -1,30 +1,26 @@
+import { useEffect, useState } from 'react'
 import Message from './Message'
+import { useChat } from '../../context/ChatContext'
+import { doc, onSnapshot } from 'firebase/firestore'
+import { database } from '../../config/firebase'
 
-function Messages() {
+const Messages: React.FC = () => {
+  const [messages, setMessages] = useState([])
+  const { data } = useChat()
+
+  useEffect(() => {
+    const unSub = onSnapshot(doc(database, 'chats', data.chatId), (doc) => {
+      doc.exists() && setMessages(doc.data().messages)
+    })
+
+    return () => unSub()
+  }, [data.chatId])
+
   return (
     <div className='flex flex-col p-2 h-[calc(100%-90px)] overflow-y-scroll'>
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
+      {messages.map((message, key) => (
+        <Message key={key} message={message} />
+      ))}
     </div>
   )
 }
