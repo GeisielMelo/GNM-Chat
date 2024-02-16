0d18c686dd57e5114ca974820a0555c14c515e0a
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2024-02-16 14:13:53 -0300

    feat: finished contact component

--- src/components/Sidebar/Contact.tsx
+++ src/components/Sidebar/Contact.tsx
@@ -1,10 +1,24 @@
-const Contact: React.FC = () => {
+type ContactProps = {
+  avatar: string
+  name: string
+  message: string
+  onClick: () => void
+}
+
+const Contact: React.FC<ContactProps> = ({ avatar, name, message, onClick }) => {
   return (
-    <div className='flex items-center gap-2 p-2 rounded active:bg-zinc-400'>
-      <img className='w-9 h-9 rounded-full object-cover' src='' alt='user image' />
+    <div className='flex items-center gap-2 p-2 rounded active:bg-zinc-400' onClick={onClick}>
+      {avatar != 'N/A' ? (
+        <img className='w-9 h-9 rounded-full object-cover' src={avatar} alt='user image' />
+      ) : (
+        <h1 className='flex items-center justify-center capitalize w-9 h-9 rounded-full text-white bg-zinc-400'>
+          {name ? name[0] : 'N/A'}
+        </h1>
+      )}
+
       <div>
-        <h1 className='text-sm'>Lorem de Souza</h1>
-        <p className='text-xs'>Lorem ipsum dolor sit amet consectetur...</p>
+        <h1 className='text-sm capitalize'>{name}</h1>
+        {message && <p className='text-xs'>{message}</p>}
       </div>
     </div>
   )
