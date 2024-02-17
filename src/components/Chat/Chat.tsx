66f1dad38435f508696069c4dfea017e8ea5af87
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2024-02-17 16:39:48 -0300

    feat: add welcoming component.

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -2,19 +2,20 @@ import { useChat } from '../../context/ChatContext'
 import Input from './Input'
 import Messages from './Messages'
 import Person from './Person'
+import Welcoming from './Welcoming'
 
 const Chat: React.FC = () => {
   const { data } = useChat()
 
-  return (
+  return data.chatId == 'null' ? (
+    <div className='flex flex-col items-center justify-center w-full rounded-r-lg bg-[#efefef]'>
+      <Welcoming />
+    </div>
+  ) : (
     <div className='flex flex-col gap-1 w-full rounded-r-lg bg-[#efefef]'>
-      {data.chatId != 'null' && (
-        <>
-          <Person avatar={data.user.photoURL} name={data.user.displayName} />
-          <Messages />
-          <Input />
-        </>
-      )}
+      <Person avatar={data.user.photoURL} name={data.user.displayName} />
+      <Messages />
+      <Input />
     </div>
   )
 }
