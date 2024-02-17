17019e5f2b8b7ad54305674b63da60978b3cd051
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2024-02-17 17:05:22 -0300

    feat: footer info.

--- src/components/Chat/Welcoming.tsx
+++ src/components/Chat/Welcoming.tsx
@@ -1,3 +1,4 @@
+import { Lock } from 'lucide-react'
 import logo from '../../assets/logo.svg'
 
 const Welcoming = () => {
@@ -6,6 +7,10 @@ const Welcoming = () => {
       <img className='max-w-20' src={logo} alt='image referring to the application logo' />
       <h1>GNM - Chat</h1>
       <p>Send and receive messages anytime, anywhere.</p>
+      <div className='flex items-center fixed bottom-8 gap-1 text-[12px] text-black/50'>
+        <Lock size={10} />
+        <p> End-to-end encryption.</p>
+      </div>
     </div>
   )
 }
