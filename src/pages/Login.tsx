19c6fe15f6b5c20ea1361f4602443e3f97241eb3
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2024-04-03 15:17:19 -0300

    refactor: refactored login form for more security.

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,34 +1,60 @@
-import { FormEvent, useState } from 'react'
+import { useState } from 'react'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from 'zod'
 import { Link, useNavigate } from 'react-router-dom'
 import { useAuth } from '../context/AuthContext'
 import logo from '../assets/logo.svg'
 import { Spinner } from '../components/Spinner'
+import { FirebaseError } from 'firebase/app'
 
-type AuthError = {
-  code: string
-}
+const formSchema = z.object({
+  email: z.string().email({ message: 'Invalid email address' }),
+  password: z.string().min(10, { message: 'Password must be at least 10 characters long' }),
+})
 
 const Login: React.FC = () => {
   const navigate = useNavigate()
   const { signIn } = useAuth()
-  const [loading, setLoading] = useState<boolean>(false)
-  const [data, setData] = useState({ email: '', password: '' })
-  const [err, setErr] = useState<string>('')
+  const [loading, setLoading] = useState(false)
+  const [err, setErr] = useState('')
 
-  const handleSubmit = async (e: FormEvent<HTMLFormElement>): Promise<void> => {
-    e.preventDefault()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  })
+
+  const showError = (code: string) => {
+    let message = ''
+    const pattern = /\/([a-z-]+)/
+    const match = pattern.exec(code)
+
+    if (match) {
+      message = match[1].replace(/-/g, ' ')
+      message = message.replace(/^\w/, (c) => c.toUpperCase())
+    }
+
+    setErr(message)
+    setTimeout(() => {
+      setErr('')
+    }, 5000)
+  }
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
     try {
       setLoading(true)
-      await signIn(data.email, data.password)
+      await signIn(values.email, values.password)
       navigate('/')
     } catch (error) {
-      if ((error as AuthError).code === 'auth/invalid-credential') {
-        setErr('Invalid E-mail or Password.')
-      }
-
-      if ((error as AuthError).code === 'auth/too-many-requests') {
-        setErr('Too many login attempts.')
-      }
+      if (error instanceof FirebaseError) showError(error.code)
+      else showError('auth/something-went-wrong.')
     } finally {
       setLoading(false)
     }
@@ -39,33 +65,48 @@ const Login: React.FC = () => {
       <div className='flex flex-col p-8 gap-4 items-center max-w-96 w-full rounded-md border shadow shadow-black/30'>
         <img className='w-12' src={logo} alt='app logo' />
         <span>Login</span>
-        {err && <p className='text-red-500'>{err}</p>}
-        <form className='flex flex-col w-full gap-2' onSubmit={handleSubmit}>
-          <input
-            className='px-2 py-1 w-full border border-zinc-400 rounded-md disabled:cursor-not-allowed'
-            type='email'
-            name='email'
-            autoComplete='on'
-            placeholder='E-mail'
-            value={data.email}
-            onChange={(e) => setData({ ...data, email: e.target.value })}
-            disabled={loading}
-          />
-          <input
-            className='px-2 py-1 w-full border border-zinc-400 rounded-md disabled:cursor-not-allowed'
-            type='password'
-            name='password'
-            autoComplete='on'
-            placeholder='Password'
-            value={data.password}
-            onChange={(e) => setData({ ...data, password: e.target.value })}
-            disabled={loading}
-          />
+
+        <form className='flex flex-col w-full gap-2' onSubmit={handleSubmit(onSubmit)}>
+          <div className='flex flex-col'>
+            <input
+              {...register('email')}
+              className='px-2 py-1 w-full border border-slate-400 rounded-lg'
+              type='email'
+              placeholder='E-mail'
+              disabled={loading}
+              autoComplete='off'
+            />
+            {errors.email && (
+              <span className='text-[0.8rem] font-medium text-red-500'>{errors.email.message}</span>
+            )}
+          </div>
+
+          <div className='flex flex-col'>
+            <input
+              {...register('password')}
+              className='px-2 py-1 w-full border border-slate-400 rounded-lg'
+              type='password'
+              placeholder='Password'
+              disabled={loading}
+              autoComplete='off'
+            />
+            {errors.password && (
+              <span className='text-[0.8rem] font-medium text-red-500'>
+                {errors.password.message}
+              </span>
+            )}
+          </div>
+
+          {err && (
+            <p className='normal-case text-center text-[0.8rem] font-medium text-red-500'>{err}</p>
+          )}
+
           <button
+            type='submit'
             className='flex justify-center p-1 rounded border shadow shadow-[#49A348] bg-[#D9FDD3] hover:bg-[#D9FDD3] text-[#49A348] transition-all disabled:cursor-progress'
             disabled={loading}
           >
-            {loading ? <Spinner /> : 'Login'}
+            {loading ? <Spinner /> : 'Register'}
           </button>
         </form>
         <p className='text-sm'>
